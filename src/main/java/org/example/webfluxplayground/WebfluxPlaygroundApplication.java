6851415d9bf0d08206f734044be3d41ffd340cf5
yongckim <zxcv9203@naver.com>
2024-06-14 22:14:12 +0900

    :sparkles: webflux 기반 요청 예시

--- src/main/java/org/example/webfluxplayground/WebfluxPlaygroundApplication.java
+++ src/main/java/org/example/webfluxplayground/WebfluxPlaygroundApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.time.LocalDateTime;
 
@@ -19,22 +21,38 @@ public class WebfluxPlaygroundApplication {
     }
 
     @Bean
-    public CommandLineRunner run() {
+    public CommandLineRunner runWithMvc() {
         return args -> {
             LocalDateTime start = LocalDateTime.now();
-            log.info("> 요청 시작 : {}", start);
+            log.info("> mvc 요청 시작 : {}", start);
             RestClient restClient = RestClient.create();
 
             for (int i = 0; i < 5; i++) {
                 ResponseEntity<String> response = restClient.get()
-                        .uri("http://localhost:8080/external-api")
+                        .uri("http://localhost:8080/")
                         .retrieve()
                         .toEntity(String.class);
                 log.info("응답 받음 : {} {}번째", response.getBody(), i + 1);
             }
             LocalDateTime end = LocalDateTime.now();
-            log.info("> 요청 종료 : {}", end);
-            log.info("총 걸린 시간 : {}초", (end.getSecond() - start.getSecond()));
+            log.info("> mvc 요청 종료 : {}", end);
+        };
+    }
+
+    @Bean
+    public CommandLineRunner runWithWebflux() {
+        return args -> {
+            log.info("> webflux 요청 시작 : {}", LocalDateTime.now());
+            WebClient webClient = WebClient.create();
+
+            for (int i = 0; i < 5; i++) {
+                int finalI = i;
+                webClient.get()
+                        .uri("http://localhost:8080/webflux")
+                        .retrieve()
+                        .bodyToMono(String.class)
+                        .subscribe(string -> log.info("응답 받음 : {} {}번째 시간 : {}", string, finalI + 1, LocalDateTime.now()));
+            }
         };
     }
 
---
+++ src/main/java/org/example/webfluxplayground/controller/WebFluxController.java
@@ -0,0 +1,28 @@
+package org.example.webfluxplayground.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/webflux")
+public class WebFluxController {
+
+    private final WebClient webClient = WebClient.create();
+
+    @GetMapping
+    public Mono<String> get() {
+        return webClient.get()
+                .uri("http://localhost:8080/webflux/external-api")
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    @GetMapping("/external-api")
+    public Mono<String> getExternalApi() throws InterruptedException {
+        Thread.sleep(5000);
+        return Mono.just("Hello from external API");
+    }
+}

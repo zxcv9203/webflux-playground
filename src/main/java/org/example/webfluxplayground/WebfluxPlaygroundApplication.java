5e756e10f70e39d2e1e4c2532f8092b5d722e28b
yongckim <zxcv9203@naver.com>
2024-06-14 21:13:10 +0900

    :tada: 프로젝트 초기화

---
+++ src/main/java/org/example/webfluxplayground/WebfluxPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package org.example.webfluxplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxPlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/webfluxplayground/WebfluxPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.webfluxplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

940b9ed5217f7e899d68eb9ad687901f83459b71
yongckim <zxcv9203@naver.com>
2024-06-27 23:16:07 +0900

    :sparkles: cold sequence http 통신 예시 작성

---
+++ src/main/java/org/example/webfluxplayground/sequence/cold/HttpColdSequenceExample.java
@@ -0,0 +1,40 @@
+package org.example.webfluxplayground.sequence.cold;
+
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+
+@Slf4j
+public class HttpColdSequenceExample {
+    public static void main(String[] args) throws Exception {
+        URI worldTimeUri = UriComponentsBuilder.newInstance().scheme("http")
+                .host("worldtimeapi.org")
+                .path("/api/timezone/Asia/Seoul")
+                .build()
+                .encode()
+                .toUri();
+
+        Mono<String> mono = getWorldTime(worldTimeUri);
+        mono.subscribe(dateTime -> log.info("# dateTime 1: {}", dateTime));
+        Thread.sleep(2000);
+        mono.subscribe(dateTime -> log.info("# dateTime 2: {}", dateTime));
+        Thread.sleep(2000);
+    }
+
+    private static Mono<String> getWorldTime(URI worldTimeUri) {
+        return WebClient.create()
+                .get()
+                .uri(worldTimeUri)
+                .retrieve()
+                .bodyToMono(String.class)
+                .map(response -> {
+                    DocumentContext jsonContext = JsonPath.parse(response);
+                    return jsonContext.read("$.datetime");
+                });
+    }
+}

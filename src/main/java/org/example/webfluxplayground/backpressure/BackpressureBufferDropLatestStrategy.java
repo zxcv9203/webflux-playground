2a48b5baabfb3ae504ce0667f6972d0686ab5d24
yongckim <zxcv9203@naver.com>
2024-06-30 18:54:37 +0900

    :sparkles: reactor의 BUFFER DROP_LATEST 전략 예제

---
+++ src/main/java/org/example/webfluxplayground/backpressure/BackpressureBufferDropLatestStrategy.java
@@ -0,0 +1,30 @@
+package org.example.webfluxplayground.backpressure;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.BufferOverflowStrategy;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.Duration;
+
+@Slf4j
+public class BackpressureBufferDropLatestStrategy {
+    public static void main(String[] args) throws InterruptedException {
+        Flux.interval(Duration.ofMillis(300L))
+                .doOnNext(data -> log.info("# emitted by original Flux: {}", data))
+                .onBackpressureBuffer(2,
+                        dropped -> log.info("# overflow & dropped: {}", dropped),
+                        BufferOverflowStrategy.DROP_LATEST)
+                .doOnNext(data -> log.info("[ # emitted by Buffer : {} ]", data))
+                .publishOn(Schedulers.parallel(), false, 1)
+                .subscribe(data -> {
+                            try {
+                                Thread.sleep(1000L);
+                            } catch (InterruptedException ignore) {
+                            }
+                            log.info("# onNext : {}", data);
+                        },
+                        error -> log.error("# onError", error));
+        Thread.sleep(3000L);
+    }
+}

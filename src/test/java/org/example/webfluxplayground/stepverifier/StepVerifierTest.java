ac716b81707093468ac7b9bab5b65f2cb030cf5e
yongckim <zxcv9203@naver.com>
2024-07-22 00:40:22 +0900

    :white_check_mark: 시간 기반 테스트 작성

--- src/test/java/org/example/webfluxplayground/stepverifier/StepVerifierTest.java
+++ src/test/java/org/example/webfluxplayground/stepverifier/StepVerifierTest.java
@@ -7,6 +7,11 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 import reactor.test.StepVerifierOptions;
+import reactor.test.scheduler.VirtualTimeScheduler;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
+
+import java.time.Duration;
 
 public class StepVerifierTest {
 
@@ -70,6 +75,76 @@ public class StepVerifierTest {
                     .expectNext(0)
                     .expectNextCount(498)
                     .expectNext(500)
+                    .expectComplete()
+                    .verify();
+        }
+    }
+
+    @Nested
+    class TimeBased {
+        public static Flux<Tuple2<String, Integer>> getCOVID19Count(Flux<Long> source) {
+            return source
+                    .flatMap(notUse -> Flux.just(
+                            Tuples.of("서울", 10),
+                            Tuples.of("경기도", 5),
+                            Tuples.of("강원도", 3),
+                            Tuples.of("충청도", 6),
+                            Tuples.of("경상도", 5),
+                            Tuples.of("전라도", 8),
+                            Tuples.of("인천", 2),
+                            Tuples.of("대전", 1),
+                            Tuples.of("대구", 2),
+                            Tuples.of("부산", 3),
+                            Tuples.of("제주도", 0)
+                    ));
+        }
+
+        public static Flux<Tuple2<String, Integer>> getVoteCount(Flux<Long> source) {
+            return source
+                    .zipWith(Flux.just(
+                            Tuples.of("중구", 15400),
+                            Tuples.of("서초구", 20020),
+                            Tuples.of("강서구", 32040),
+                            Tuples.of("강동구", 14506),
+                            Tuples.of("서대문구", 35650)
+                    ))
+                    .map(Tuple2::getT2);
+        }
+
+        @Test
+        @DisplayName("타임 기반 테스트 1")
+        void test1() {
+            StepVerifier.withVirtualTime(() -> getCOVID19Count(Flux.interval(Duration.ofHours(1)).take(1)))
+                    .expectSubscription()
+                    .then(() -> VirtualTimeScheduler
+                            .get()
+                            .advanceTimeBy(Duration.ofHours(1)))
+                    .expectNextCount(11)
+                    .expectComplete()
+                    .verify();
+        }
+
+        @Test
+        @DisplayName("타임 기반 테스트 2")
+        void test2() {
+            StepVerifier.create(getCOVID19Count(Flux.interval(Duration.ofMinutes(1)).take(1)))
+                    .expectSubscription()
+                    .expectNextCount(11)
+                    .expectComplete()
+                    .verify(Duration.ofSeconds(3));
+        }
+
+        @Test
+        @DisplayName("타임 기반 테스트 3")
+        void test3() {
+            StepVerifier.withVirtualTime(() -> getVoteCount(Flux.interval(Duration.ofMinutes(1))))
+                    .expectSubscription()
+                    .expectNoEvent(Duration.ofMinutes(1))
+                    .expectNoEvent(Duration.ofMinutes(1))
+                    .expectNoEvent(Duration.ofMinutes(1))
+                    .expectNoEvent(Duration.ofMinutes(1))
+                    .expectNoEvent(Duration.ofMinutes(1))
+                    .expectNextCount(5)
                     .expectComplete()
                     .verify();
         }

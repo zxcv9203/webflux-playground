9821009e06b1fe01ea62812ec9b8b00a9a664f3a
yongckim <zxcv9203@naver.com>
2024-07-23 22:08:48 +0900

    :white_check_mark: 오동작하는 TestPublisher 테스트

--- src/test/java/org/example/webfluxplayground/testpublisher/TestPublisherTest.java
+++ src/test/java/org/example/webfluxplayground/testpublisher/TestPublisherTest.java
@@ -6,6 +6,8 @@ import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 import reactor.test.publisher.TestPublisher;
 
+import java.util.List;
+
 public class TestPublisherTest {
 
     @Nested
@@ -22,6 +24,31 @@ public class TestPublisherTest {
                     .then(() -> source.emit(2, 4, 6, 8, 10))
                     .expectNext(1, 2, 3, 4)
                     .expectError()
+                    .verify();
+        }
+    }
+
+    @Nested
+    class MisbehavingTestPublisher {
+        public static Flux<Integer> divideByTwo(Flux<Integer> source) {
+            return source.zipWith(Flux.just(2, 2, 2, 2, 0), (x, y) -> x / y);
+        }
+        private static List<Integer> getDataSource() {
+            return List.of(2, 4, 6, 8, null);
+        }
+        @Test
+        void divideByTwoTest() {
+            TestPublisher<Integer> source = TestPublisher.createNoncompliant(TestPublisher.Violation.ALLOW_NULL);
+
+            StepVerifier.create(divideByTwo(source.flux()))
+                    .expectSubscription()
+                    .then(() -> {
+                        getDataSource()
+                                .forEach(source::next);
+                        source.complete();
+                    })
+                    .expectNext(1, 2, 3, 4, 5)
+                    .expectComplete()
                     .verify();
         }
     }

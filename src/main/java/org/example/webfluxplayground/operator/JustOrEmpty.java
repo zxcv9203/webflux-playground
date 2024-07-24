711c66639b96c03f9bb52bdd15752adbe9bbd092
yongckim <zxcv9203@naver.com>
2024-07-24 20:29:26 +0900

    :sparkles: justOrEmpty Operator 예시

---
+++ src/main/java/org/example/webfluxplayground/operator/JustOrEmpty.java
@@ -0,0 +1,18 @@
+package org.example.webfluxplayground.operator;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+public class JustOrEmpty {
+    public static void main(String[] args) {
+        Mono.justOrEmpty(null)
+                .subscribe(
+                        data -> {
+                        },
+                        error -> {
+                        },
+                        () -> log.info("# onComplete")
+                );
+    }
+}

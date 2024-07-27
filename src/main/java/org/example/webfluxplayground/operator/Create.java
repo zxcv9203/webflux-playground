fcae8c2fc91c675d43459eb85133782255fc1280
yongckim <zxcv9203@naver.com>
2024-07-27 22:51:24 +0900

    :sparkles: Create Operator 예시

---
+++ src/main/java/org/example/webfluxplayground/operator/Create.java
@@ -0,0 +1,58 @@
+package org.example.webfluxplayground.operator;
+
+import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+
+import java.util.List;
+
+@Slf4j
+public class Create {
+    private static int SIZE = 0;
+    private static int COUNT = -1;
+
+    private static final List<Integer> DATA_SOURCE = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    public static void main(String[] args) {
+        log.info("# start");
+
+        Flux.create((FluxSink<Integer> sink) -> {
+            sink.onRequest(n -> {
+                try {
+                    Thread.sleep(1000L);
+                    for (int i = 0; i < n; i++) {
+                        if (COUNT >= 9) {
+                            sink.complete();
+                        } else {
+                            COUNT++;
+                            sink.next(DATA_SOURCE.get(COUNT));
+                        }
+                    }
+                } catch (InterruptedException ignored) {
+                }
+            });
+            sink.onDispose(() -> log.info("# clean up"));
+        }).subscribe(new BaseSubscriber<Integer>() {
+            @Override
+            protected void hookOnSubscribe(Subscription subscription) {
+                request(2);
+            }
+
+            @Override
+            protected void hookOnNext(Integer value) {
+                SIZE++;
+                log.info("# onNext : {}", value);
+                if (SIZE == 2) {
+                    request(2);
+                    SIZE = 0;
+                }
+            }
+
+            @Override
+            protected void hookOnComplete() {
+                log.info("# onComplete");
+            }
+        });
+    }
+}

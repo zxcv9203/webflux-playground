d56cc6e0dd9035c78efa47b23b5bbb2f340317e7
yongckim <zxcv9203@naver.com>
2024-08-04 20:05:30 +0900

    :fire: 미사용 요청 주석처리

--- src/main/java/org/example/webfluxplayground/WebfluxPlaygroundApplication.java
+++ src/main/java/org/example/webfluxplayground/WebfluxPlaygroundApplication.java
@@ -20,40 +20,40 @@ public class WebfluxPlaygroundApplication {
         SpringApplication.run(WebfluxPlaygroundApplication.class, args);
     }
 
-    @Bean
-    public CommandLineRunner runWithMvc() {
-        return args -> {
-            LocalDateTime start = LocalDateTime.now();
-            log.info("> mvc 요청 시작 : {}", start);
-            RestClient restClient = RestClient.create();
-
-            for (int i = 0; i < 5; i++) {
-                ResponseEntity<String> response = restClient.get()
-                        .uri("http://localhost:8080/")
-                        .retrieve()
-                        .toEntity(String.class);
-                log.info("응답 받음 : {} {}번째", response.getBody(), i + 1);
-            }
-            LocalDateTime end = LocalDateTime.now();
-            log.info("> mvc 요청 종료 : {}", end);
-        };
-    }
-
-    @Bean
-    public CommandLineRunner runWithWebflux() {
-        return args -> {
-            log.info("> webflux 요청 시작 : {}", LocalDateTime.now());
-            WebClient webClient = WebClient.create();
-
-            for (int i = 0; i < 5; i++) {
-                int finalI = i;
-                webClient.get()
-                        .uri("http://localhost:8080/webflux")
-                        .retrieve()
-                        .bodyToMono(String.class)
-                        .subscribe(string -> log.info("응답 받음 : {} {}번째 시간 : {}", string, finalI + 1, LocalDateTime.now()));
-            }
-        };
-    }
+//    @Bean
+//    public CommandLineRunner runWithMvc() {
+//        return args -> {
+//            LocalDateTime start = LocalDateTime.now();
+//            log.info("> mvc 요청 시작 : {}", start);
+//            RestClient restClient = RestClient.create();
+//
+//            for (int i = 0; i < 5; i++) {
+//                ResponseEntity<String> response = restClient.get()
+//                        .uri("http://localhost:8080/")
+//                        .retrieve()
+//                        .toEntity(String.class);
+//                log.info("응답 받음 : {} {}번째", response.getBody(), i + 1);
+//            }
+//            LocalDateTime end = LocalDateTime.now();
+//            log.info("> mvc 요청 종료 : {}", end);
+//        };
+//    }
+//
+//    @Bean
+//    public CommandLineRunner runWithWebflux() {
+//        return args -> {
+//            log.info("> webflux 요청 시작 : {}", LocalDateTime.now());
+//            WebClient webClient = WebClient.create();
+//
+//            for (int i = 0; i < 5; i++) {
+//                int finalI = i;
+//                webClient.get()
+//                        .uri("http://localhost:8080/webflux")
+//                        .retrieve()
+//                        .bodyToMono(String.class)
+//                        .subscribe(string -> log.info("응답 받음 : {} {}번째 시간 : {}", string, finalI + 1, LocalDateTime.now()));
+//            }
+//        };
+//    }
 
 }
